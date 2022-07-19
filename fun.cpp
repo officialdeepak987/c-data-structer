602726c4d776dd73a48e8c078ab7879a48183875
Deepak Singh <deepak5032singh@gmail.com>
2022-07-19 14:08:34 +0530

    function

---
+++ fun.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ main(){
+	int a;
+	cout<<a;
+//	cin>>a;
+   
+	
+}
+int fun (int n){
+	int sum=0;
+	cout<<"Enter any number :/n";
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	sum=sum+i;
+
+return sum;
+}
